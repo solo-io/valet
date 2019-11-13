79615d2d06f597aa905fe5e97ebf4cf9dcbc4588
Rick Ducott <rick.ducott@solo.io>
2019-11-13 14:42:46 -0500

    Perf speed up, don't download file if already exists.
    Cuts 20-25 seconds off test

--- cli/internal/ensure/resource/application/helm_chart.go
+++ cli/internal/ensure/resource/application/helm_chart.go
@@ -44,11 +44,18 @@ func (h *HelmChart) addHelmRepo(ctx context.Context, input render.InputParams) e
 func (h *HelmChart) fetchChart(ctx context.Context, input render.InputParams) (string, error) {
 	downloadDir, err := h.getLocalDirectory()
 	if err != nil {
-		cmd.Stderr().Println("Error determining local directory for extracting chart: %s", err.Error())
 		return "", err
+	}
+	downloadPath := h.getDownloadPath(downloadDir)
+	if exists, err := fileExists(downloadPath); err == nil && exists {
+		cmd.Stdout().Println("Chart already downloaded to %s", downloadPath)
+		return downloadPath, nil
 	}
 	if err := os.MkdirAll(downloadDir, os.ModePerm); err != nil {
 		cmd.Stderr().Println("Error making directory: %s", err.Error())
+		return "", err
+	}
+	if err := h.addHelmRepo(ctx, input); err != nil {
 		return "", err
 	}
 	command := cmd.New().
@@ -63,14 +70,31 @@ func (h *HelmChart) fetchChart(ctx context.Context, input render.InputParams) (s
 		cmd.Stderr().Println(out)
 		return "", err
 	}
-	downloadPath := filepath.Join(downloadDir, fmt.Sprintf("%s-%s.tgz", h.ChartName, h.Version))
 	cmd.Stdout().Println("Successfully downloaded chart %s", downloadPath)
 	return downloadPath, nil
+}
+
+func fileExists(path string) (bool, error) {
+	file, err := os.Stat(path)
+	if err != nil && !os.IsNotExist(err) {
+		return false, err
+	} else if err != nil {
+		return false, nil
+	}
+	if file.IsDir() {
+		return false, errors.Errorf("Unexpected directory %s", path)
+	}
+	return true, nil
+}
+
+func (h *HelmChart) getDownloadPath(downloadDir string) string {
+	return filepath.Join(downloadDir, fmt.Sprintf("%s-%s.tgz", h.ChartName, h.Version))
 }
 
 func (h *HelmChart) getLocalDirectory() (string, error) {
 	home, err := os.UserHomeDir()
 	if err != nil {
+		cmd.Stderr().Println("Error determining local directory for extracting chart: %s", err.Error())
 		return "", err
 	}
 	return filepath.Join(home, ".helm", "cache", "valet", h.RepoName), nil
@@ -78,9 +102,6 @@ func (h *HelmChart) getLocalDirectory() (string, error) {
 
 func (h *HelmChart) Render(ctx context.Context, input render.InputParams) (kuberesource.UnstructuredResources, error) {
 	if err := input.RenderFields(h); err != nil {
-		return nil, err
-	}
-	if err := h.addHelmRepo(ctx, input); err != nil {
 		return nil, err
 	}
 	downloadPath, err := h.fetchChart(ctx, input)

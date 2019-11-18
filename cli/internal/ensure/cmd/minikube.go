3110f467613c1dfbaa82c182ef07b23ec80c808a
EItanya <eitan.yarmush@solo.io>
2019-11-18 09:27:27 -0800

    minikube output and options

--- cli/internal/ensure/cmd/minikube.go
+++ cli/internal/ensure/cmd/minikube.go
@@ -3,6 +3,8 @@ package cmd
 import (
 	"context"
 	"fmt"
+
+	"github.com/solo-io/go-utils/errors"
 )
 
 type Minikube struct {
@@ -23,20 +25,19 @@ func (m *Minikube) Cmd() *Command {
 	return m.cmd
 }
 
-func (m *Minikube) Delete() *Minikube {
-	return m.With("delete")
-}
-
 func (m *Minikube) Status() *Minikube {
 	return m.With("status")
 }
 
-func (m *Minikube) Start() *Minikube {
-	return m.With("start")
-}
-
 func (m *Minikube) Memory(mb int) *Minikube {
 	return m.With(fmt.Sprintf("--memory=%d", mb))
+}
+
+func (m *Minikube) VmDriver(vmDriver string) *Minikube {
+	if vmDriver == "" {
+		return m
+	}
+	return m.With(fmt.Sprintf("--vm-driver=%s", vmDriver))
 }
 
 func (m *Minikube) Cpus(cpus int) *Minikube {
@@ -49,4 +50,22 @@ func (m *Minikube) KubeVersion(kubeVersion string) *Minikube {
 
 func (m *Minikube) IP(ctx context.Context, runner Runner) (string, error) {
 	return runner.Output(ctx, m.With("ip").Cmd())
+}
+
+func (m *Minikube) Start(ctx context.Context, runner Runner) error {
+	streamHandler, err := runner.Stream(ctx, m.With("start").Cmd())
+	if err != nil {
+		return err
+	}
+	inputErr := errors.New("could not delete minikube cluster")
+	return streamHandler.StreamHelper(inputErr)
+}
+
+func (m *Minikube) Delete(ctx context.Context, runner Runner) error {
+	streamHandler, err := runner.Stream(ctx, m.With("delete").Cmd())
+	if err != nil {
+		return err
+	}
+	inputErr := errors.New("could not delete minikube cluster")
+	return streamHandler.StreamHelper(inputErr)
 }
